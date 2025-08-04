86dfd3a636dbd84c08595ff3d0eb288874ef5a03
kilaramd21 <115880508+kilaramd21@users.noreply.github.com>
2025-08-05 01:10:37 +0530

    Update IntroToSpringbootApplication.java

--- src/main/java/com/spring/week1/intro_to_springboot/IntroToSpringbootApplication.java
+++ src/main/java/com/spring/week1/intro_to_springboot/IntroToSpringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IntroToSpringbootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world yoo lala edit");
+		System.out.println("hello world yoo lala edit fetch");
 		SpringApplication.run(IntroToSpringbootApplication.class, args);
 	}
 
