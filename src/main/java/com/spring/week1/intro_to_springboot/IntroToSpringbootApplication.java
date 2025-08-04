a565d627e5c87d2f519e3797a723aefce8ed691d
deepika <deepikakilaram@gmail.com>
2025-08-05 00:32:29 +0530

    print statement added

--- src/main/java/com/spring/week1/intro_to_springboot/IntroToSpringbootApplication.java
+++ src/main/java/com/spring/week1/intro_to_springboot/IntroToSpringbootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IntroToSpringbootApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(IntroToSpringbootApplication.class, args);
 	}
 
