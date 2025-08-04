e09fc06077e49296791787ca2482e8f5e1f05841
deepika <deepikakilaram@gmail.com>
2025-08-05 00:25:20 +0530

    store changes

---
+++ src/main/java/com/spring/week1/intro_to_springboot/IntroToSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.week1.intro_to_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/week1/intro_to_springboot/IntroToSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.week1.intro_to_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
